c3f0d9fa3fbb150b6c3e3075b873f1fe8d0f11e5
Feinte75 <glenn.feunteun@telecom-bretagne.eu>
2014-06-02 22:35:54 +0200

    Poursuite traduction commentaires. Optimisation méthode calcul karma. Suppression affichage password avec methode tostring de Member

--- avis/Item.java
+++ avis/Item.java
@@ -31,7 +31,7 @@ public abstract class Item {
 	}
 	
 	/**
-	 * Compare param title to attribute title
+	 * Compare parameters title to attribute title
 	 * @param title the title of the item searched
 	 * @return the item if it is found, null either
 	 */
@@ -48,12 +48,14 @@ public abstract class Item {
 		
 		float av = 0;
 		float karmaWeight = 0;
+		float karmaMember = 0;
 		
 		for(Review sum : reviews){
+			karmaMember = sum.getMember().getKarma();
 			// Compute the rating of each reviews with the karma of their authors
-			av += (sum.getRating() * sum.getMember().getKarma());
-			// Save the total amount of karma for the members who reviewed the item
-			karmaWeight += sum.getMember().getKarma();
+			av += (sum.getRating() * karmaMember);
+			// Save the total amount of karma of the members who reviewed the item
+			karmaWeight += karmaMember;
 		}
 		// Divide by the total karma to make people with good karma more influent on the rating
 		av /= karmaWeight;
